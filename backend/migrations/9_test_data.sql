59257d9f460fce905cf40ac4c9faa171ef629671
Remi1095 <remi.grosjean1@gmail.com>
2025-02-14 14:51:43 -0500

    removed tests folder

--- backend/migrations/9_test_data.sql
+++ backend/migrations/9_test_data.sql
@@ -1,4 +1,10 @@
+-- Active: 1738094970568@@127.0.0.1@5432@user@public
 INSERT INTO
     app_user (username, password_hash)
 VALUES ('admin', 'password');
+
+
+INSERT INTO meta_field (table_id, name, options)
+VALUES (1, 'Test Field', '{"type": "text", "is_required": false}'::json)
+RETURNING field_id, data_field_name;
 
