261a087edfa515be13c77957184d3f7b8d37ba84
J-Dufour <dufour.dev@proton.me>
2025-02-17 19:52:04 -0500

    refactor: change condition for table rendering in preparation for field editor

--- frontend/src/routes/tables/+page.svelte
+++ frontend/src/routes/tables/+page.svelte
@@ -15,10 +15,20 @@
   let { data }: PageProps = $props();
 
   let curTable = $state(null as unknown as Table)
+
+  const EditMode = {
+    NONE: 0,
+    TABLE: 1,
+    FIELDS: 2
+  }
+
+  let editMode = $state(EditMode.NONE)
 </script>
 
 
 <div class="flex gap-4 p-4 size-full">
+
+  <!-- Sidebar -->
   <div class="basis-1/3 bg-gray-200 rounded-lg p-3 size-full">
     <h2>Tables</h2>
     <div class="flex flex-col">
@@ -26,23 +36,31 @@
         Loading...
       {:then tables}
         {#each tables as t}
-          <button onclick={() => {curTable = t}} 
+          <button onclick={() => {curTable = t; editMode = EditMode.TABLE}} 
             class="text-left bg-gray-200 hover:bg-gray-400 transition duration-300 rounded-md p-2">{t.name}</button>
         {/each}
       {/await}
     </div>
     <button class="text-center w-full rounded-xl p-2 hover:p-3 transition-size duration-300 border-2 border-dashed border-black">Add Table</button>
   </div>
-   <div class="bg-gray-200 rounded-lg p-3 size-full flex flex-col items-center">
-    {#if curTable === null}
+
+  <!-- Main Editor -->
+  <div class="bg-gray-200 rounded-lg p-3 size-full flex flex-col items-center">
+    {#if editMode === EditMode.NONE}
       <h2 class="text-lg font-bold">Select a Table</h2>
-    {:else}
+    {:else if editMode === EditMode.TABLE}
+      <!-- Top Bar -->
       <div class="flex items-center gap-2 mb-2">
         <h2 class="text-lg font-bold">{curTable.name}</h2>
-        <button class="px-2 bg-white hover:bg-gray-100 transition duration-300 rounded-md">Edit</button>
+        <button onclick={()=>{editMode = EditMode.FIELDS}} class="px-2 bg-white hover:bg-gray-100 transition duration-300 rounded-md">Edit</button>
         <button class="px-2 bg-red-400 hover:bg-red-500 transition duration-300 rounded-md">Delete</button>
       </div>
+
+      <!-- Main Table -->
       <DataTable table_prop={curTable}/>
+    {:else if editMode === EditMode.FIELDS}
+      <p>Placeholder</p>
+    
     {/if}
   </div>
 </div>
