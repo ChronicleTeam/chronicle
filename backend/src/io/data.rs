e1e62ad30dd396ef596e43884388741605ce1d43
Remi1095 <remi.grosjean1@gmail.com>
2025-03-14 01:11:59 -0400

    starting xlsx import

--- backend/src/io/data.rs
+++ backend/src/io/data.rs
@@ -7,8 +7,9 @@ use umya_spreadsheet::{reader::xlsx, XlsxError};
 pub fn import_from_xlsx(path: &Path) -> Result<(), XlsxError> {
 
     let mut book = xlsx::lazy_read(path)?;
-
-    
+    for sheet in book.get_sheet_collection() {
+        sheet.get_highest_column_and_row();
+    }
 
     todo!()
 }
