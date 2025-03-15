6fd7e81b32b528fca3ef08c07a65e62478c1c868
Remi1095 <remi.grosjean1@gmail.com>
2025-03-15 18:27:31 -0400

    field conversion

--- backend/src/model/mod.rs
+++ backend/src/model/mod.rs
@@ -13,13 +13,16 @@ pub mod data;
 pub mod users;
 pub mod viz;
 
+use std::str::FromStr;
+
 use chrono::{DateTime, Utc};
 use data::FieldKind;
+use num_traits::{FromPrimitive, ToPrimitive};
 use rust_decimal::Decimal;
 use serde::{Serialize, Serializer};
 use sqlx::{
-    postgres::{PgArgumentBuffer, PgRow, PgTypeInfo},
-    Encode, Postgres, Row, Type,
+    postgres::{PgArgumentBuffer, PgRow},
+    Encode, Postgres, Row,
 };
 use viz::Aggregate;
 
@@ -79,7 +82,7 @@ impl<'q> Encode<'q, Postgres> for Cell {
     //         Cell::Null => PgTypeInfo::with_name("TEXT"), // Default type for NULL
     //     })
     // }
-    
+
     // fn encode_by_ref(
     //     &self,
     //     buf: &mut <Postgres as sqlx::Database>::ArgumentBuffer<'q>,
@@ -88,7 +91,6 @@ impl<'q> Encode<'q, Postgres> for Cell {
     // }
 }
 
-
 // impl Type<Postgres> for Cell {
 //     fn type_info() -> PgTypeInfo {
 //         PgTypeInfo::with_name("TEXT") // Default to TEXT (or JSONB if preferred)
@@ -96,14 +98,12 @@ impl<'q> Encode<'q, Postgres> for Cell {
 // }
 impl Cell {
     /// Get the `Cell` from this PostgreSQL row into the proper type based on `FieldKind`.
-    pub fn from_field_row(row: &PgRow, index: &str, field_kind: &FieldKind) -> sqlx::Result<Cell> {
+    pub fn from_field_row(row: &PgRow, index: &str, field_kind: &FieldKind) -> sqlx::Result<Self> {
         if let Ok(None) = row.try_get::<Option<bool>, _>(index) {
             return Ok(Cell::Null);
         }
         Ok(match field_kind {
-            FieldKind::Text { .. } | FieldKind::WebLink { .. } | FieldKind::Email { .. } => {
-                Cell::String(row.try_get(index)?)
-            }
+            FieldKind::Text { .. } | FieldKind::WebLink { .. } => Cell::String(row.try_get(index)?),
             FieldKind::Integer { .. }
             | FieldKind::Progress { .. }
             | FieldKind::Enumeration { .. } => Cell::Integer(row.try_get(index)?),
@@ -120,7 +120,7 @@ impl Cell {
         index: &str,
         aggregate: &Aggregate,
         field_kind: &FieldKind,
-    ) -> sqlx::Result<Cell> {
+    ) -> sqlx::Result<Self> {
         if let Ok(None) = row.try_get::<Option<bool>, _>(index) {
             return Ok(Cell::Null);
         }
@@ -133,4 +133,141 @@ impl Cell {
             Aggregate::Count => Cell::Integer(row.try_get(index)?),
         })
     }
+
+    pub fn convert_field_kind(self, field_kind: &FieldKind) -> Option<Self> {
+        match field_kind {
+            FieldKind::Text { .. } | FieldKind::WebLink { .. } => Some(Cell::String(match self {
+                Cell::Integer(v) => v.to_string(),
+                Cell::Float(v) => v.to_string(),
+                Cell::Decimal(v) => v.to_string(),
+                Cell::Boolean(v) => v.to_string(),
+                Cell::DateTime(v) => v.to_string(),
+                Cell::String(_) | Cell::Null => return Some(self),
+            })),
+            FieldKind::Integer {
+                range_start,
+                range_end,
+                ..
+            } => {
+                let v: i64 = match self {
+                    Cell::Float(v) => num_traits::cast(v)?,
+                    Cell::Decimal(v) => v.to_i64()?,
+                    Cell::Boolean(v) => v.into(),
+                    Cell::DateTime(v) => v.timestamp(),
+                    Cell::String(v) => v.parse().ok()?,
+                    Cell::Integer(_) | Cell::Null => return Some(self),
+                };
+                if in_range(&v, range_start.as_ref(), range_end.as_ref()) {
+                    Some(Cell::Integer(v))
+                } else {
+                    None
+                }
+            }
+            FieldKind::Float {
+                range_start,
+                range_end,
+                ..
+            } => {
+                let v: f64 = match self {
+                    Cell::Integer(v) => num_traits::cast(v)?,
+                    Cell::Decimal(v) => v.to_f64()?,
+                    Cell::Boolean(v) => v.into(),
+                    Cell::DateTime(_) => return None,
+                    Cell::String(v) => v.parse().ok()?,
+                    Cell::Float(_) | Cell::Null => return Some(self),
+                };
+                if in_range(&v, range_start.as_ref(), range_end.as_ref()) {
+                    Some(Cell::Float(v))
+                } else {
+                    None
+                }
+            }
+            FieldKind::Money {
+                range_start,
+                range_end,
+                ..
+            } => {
+                let v: Decimal = match self {
+                    Cell::Integer(v) => Decimal::from_i64(v)?,
+                    Cell::Float(v) => Decimal::from_f64(v)?,
+                    Cell::String(v) => v.parse().ok()?,
+                    Cell::Boolean(_) | Cell::DateTime(_) => return None,
+                    Cell::Decimal(_) | Cell::Null => return Some(self),
+                };
+                if in_range(&v, range_start.as_ref(), range_end.as_ref()) {
+                    Some(Cell::Decimal(v))
+                } else {
+                    None
+                }
+            }
+            FieldKind::Progress { total_steps } => {
+                let v: i64 = match self {
+                    Cell::Integer(v) => v,
+                    Cell::Float(v) => num_traits::cast(v)?,
+                    Cell::Decimal(v) => v.to_i64()?,
+                    Cell::Boolean(v) => v.into(),
+                    Cell::String(v) => v.parse().ok()?,
+                    Cell::DateTime(_) => return None,
+                    Cell::Null => return Some(self),
+                };
+                if v >= 0 && v <= *total_steps {
+                    Some(Cell::Integer(v))
+                } else {
+                    None
+                }
+            }
+            FieldKind::DateTime {
+                range_start,
+                range_end,
+                ..
+            } => {
+                let v: DateTime<Utc> = match self {
+                    Cell::Integer(v) => DateTime::from_timestamp(v, 0)?,
+                    Cell::String(v) => DateTime::from_str(&v).ok()?,
+                    Cell::Float(_) | Cell::Decimal(_) | Cell::Boolean(_) => return None,
+                    Cell::DateTime(_) | Cell::Null => return Some(self),
+                };
+                if in_range(&v, range_start.as_ref(), range_end.as_ref()) {
+                    Some(Cell::DateTime(v))
+                } else {
+                    None
+                }
+            }
+            FieldKind::Checkbox => Some(Cell::Boolean(match self {
+                Cell::Integer(v) => v != 0,
+                Cell::String(v) => v.parse().ok()?,
+                Cell::Float(_) | Cell::Decimal(_) | Cell::DateTime(_) => return None,
+                Cell::Boolean(_) | Cell::Null => return Some(self),
+            })),
+            FieldKind::Enumeration {
+                values,
+                default_value,
+                ..
+            } => {
+                let v: String = match self {
+                    Cell::Integer(v) => v.to_string(),
+                    Cell::Float(v) => v.to_string(),
+                    Cell::Decimal(v) => v.to_string(),
+                    Cell::Boolean(v) => v.to_string(),
+                    Cell::DateTime(v) => v.to_string(),
+                    Cell::String(v) => v,
+                    Cell::Null => return Some(self),
+                };
+                Some(
+                    if let Some((k, _)) = values.iter().find(|(_, value)| **value == v) {
+                        Cell::Integer(*k)
+                    } else {
+                        Cell::Integer(*default_value)
+                    },
+                )
+            }
+        }
+    }
+}
+
+fn in_range<T>(value: &T, range_start: Option<&T>, range_end: Option<&T>) -> bool
+where
+    T: PartialOrd,
+{
+    range_start.map_or(true, |start| value >= start) && range_end.map_or(true, |end| value <= end)
 }
