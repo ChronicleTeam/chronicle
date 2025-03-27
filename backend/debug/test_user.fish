85f945af128d547cb5fa50b44cd9ece0db935acc
Remi1095 <remi.grosjean1@gmail.com>
2025-03-27 11:03:28 -0400

    Default user and password creation

--- backend/debug/test_user.fish
+++ backend/debug/test_user.fish
@@ -2,12 +2,12 @@ set ADDR 'http://localhost:8000/api'
 
 curl -X POST $ADDR/register \
     -H "Content-Type: application/x-www-form-urlencoded" \
-    -d "username=user@example.com&password=test123" \
+    -d "username=test@example.com&password=test123" \
     -c cookies.txt -b cookies.txt
 
 curl -X POST $ADDR/login \
     -H "Content-Type: application/x-www-form-urlencoded" \
-    -d "username=user@example.com&password=test123" \
+    -d "username=test@example.com&password=test123" \
     -c cookies.txt -b cookies.txt
 
 curl -X GET $ADDR/logout \
