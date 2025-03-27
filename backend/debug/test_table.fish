e9e920a7281be57021a20c61ad61bc852ae3bbf4
Remi1095 <remi.grosjean1@gmail.com>
2025-03-27 18:48:25 -0400

    AuthSession

--- backend/debug/test_table.fish
+++ backend/debug/test_table.fish
@@ -2,71 +2,70 @@
 
 set ADDR 'http://localhost:8000/api'
 
-curl -X POST $ADDR/tables \
+curl -b cookies.txt -X POST $ADDR/tables \
     -H "Content-Type: application/json" \
-    -d '{"name": "Test Table", "description": "This is a test table"}' \
-    -b cookies.txt
+    -d '{"name": "Test Table", "description": "This is a test table"}'
 
 
-curl -X POST $ADDR/tables/1/fields \
+curl -b cookies.txt -X POST $ADDR/tables/1/fields \
     -H "Content-Type: application/json" \
     -d '{"name": "Field", "field_kind": {"type": "Text", "is_required": true}}'
 
-curl -X POST $ADDR/tables/1/fields \
+curl -b cookies.txt -X POST $ADDR/tables/1/fields \
     -H "Content-Type: application/json" \
     -d '{"name": "Field", "field_kind": {"type": "Integer", "is_required": true}}'
 
 
-curl -X POST $ADDR/tables/1/entries \
+curl -b cookies.txt -X POST $ADDR/tables/1/entries \
     -H "Content-Type: application/json" \
     -d '{"cells": {"1": "c1", "2": 1}}'
 
-curl -X POST $ADDR/tables/1/entries \
+curl -b cookies.txt -X POST $ADDR/tables/1/entries \
     -H "Content-Type: application/json" \
     -d '{"cells": {"1": "c1", "2": 2}}'
 
-curl -X POST $ADDR/tables/1/entries \
+curl -b cookies.txt -X POST $ADDR/tables/1/entries \
     -H "Content-Type: application/json" \
     -d '{"cells": {"1": "c1", "2": 3}}'
 
-curl -X POST $ADDR/tables/1/entries \
+curl -b cookies.txt -X POST $ADDR/tables/1/entries \
     -H "Content-Type: application/json" \
     -d '{"cells": {"1": "c1", "2": 4}}'
 
-curl -X POST $ADDR/tables/1/entries \
+curl -b cookies.txt -X POST $ADDR/tables/1/entries \
     -H "Content-Type: application/json" \
     -d '{"cells": {"1": "c1", "2": 5}}'
 
-curl -X POST $ADDR/tables/1/entries \
+curl -b cookies.txt -X POST $ADDR/tables/1/entries \
     -H "Content-Type: application/json" \
     -d '{"cells": {"1": "c2", "2": 10}}'
 
-curl -X POST $ADDR/tables/1/entries \
+curl -b cookies.txt -X POST $ADDR/tables/1/entries \
     -H "Content-Type: application/json" \
     -d '{"cells": {"1": "c2", "2": 15}}'
 
-curl -X POST $ADDR/tables/1/entries \
+curl -b cookies.txt -X POST $ADDR/tables/1/entries \
     -H "Content-Type: application/json" \
     -d '{"cells": {"1": "c2", "2": 20}}'
 
 
-curl -X POST $ADDR/tables/1/entries \
+curl -b cookies.txt -X POST $ADDR/tables/1/entries \
     -H "Content-Type: application/json" \
     -d '{"cells": {"1": "c3", "2": 123}}'
 
-curl -X POST $ADDR/tables/1/entries \
+curl -b cookies.txt -X POST $ADDR/tables/1/entries \
     -H "Content-Type: application/json" \
     -d '{"cells": {"1": "c3", "2": 321}}'
 
-curl -X POST $ADDR/tables \
+curl -b cookies.txt -X POST $ADDR/tables \
     -H "Content-Type: application/json" \
     -d '{"parent_id": 1, "name": "Child Table", "description": ""}'
 
 
-curl -X POST $ADDR/tables/2/fields \
+curl -b cookies.txt -X POST $ADDR/tables/2/fields \
     -H "Content-Type: application/json" \
     -d '{"name": "Field", "field_kind": {"type": "Text", "is_required": true}}'
 
-curl -X POST $ADDR/tables/2/entries \
+curl -b cookies.txt -X POST $ADDR/tables/2/entries \
     -H "Content-Type: application/json" \
     -d '{"parent_id": 1, "cells": {"3": "test"}}'
