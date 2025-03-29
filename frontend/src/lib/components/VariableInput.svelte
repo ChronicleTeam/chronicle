bd1e95311ad1bf2d0022e28d50d349c420046b10
J-Dufour <dufour.dev@proton.me>
2025-03-28 21:22:29 -0400

    refactor: simplify subtables in FieldEditor

--- frontend/src/lib/components/VariableInput.svelte
+++ frontend/src/lib/components/VariableInput.svelte
@@ -17,74 +17,76 @@
   } = $props();
 </script>
 
-{#if params.label ?? false}
-  <label class={["mr-2 min-w-28", disabled && "text-gray-300"]} for={id}
-    >{params.label}:</label
-  >
-{/if}
-{#if params.type === "select"}
-  {@const opts = Array.isArray(params.selectOptions)
-    ? params.selectOptions.map((o) => [o, o])
-    : Object.entries(params.selectOptions)}
-  <select
-    {disabled}
-    {id}
-    bind:value={params.bindGetter, params.bindSetter}
-    {onclick}
-  >
-    {#each opts as opt}
-      <option value={opt[0]}>{opt[1]}</option>
-    {/each}
-  </select>
-{:else if params.type === "textarea"}
-  <textarea
-    {disabled}
-    {id}
-    class={innerClass}
-    bind:value={params.bindGetter, params.bindSetter}
-    {onclick}
-  ></textarea>
-{:else if params.type === "checkbox"}
-  <input
-    {disabled}
-    {id}
-    type="checkbox"
-    bind:checked={params.bindGetter, params.bindSetter}
-    {onclick}
-  />
-{:else if params.type === "url"}
-  {#if disabled}
-    <a
-      href={"https://" + params.bindGetter().toString()}
-      class={["text-blue-600 underline cursor-pointer", innerClass]}
-      target="_blank">{params.bindGetter().toString()}</a
+<div class={params.label ? "flex justify-between w-full" : ""}>
+  {#if params.label ?? false}
+    <label class={["mr-2 w-auto", disabled && "text-gray-300"]} for={id}
+      >{params.label}:</label
     >
+  {/if}
+  {#if params.type === "select"}
+    {@const opts = Array.isArray(params.selectOptions)
+      ? params.selectOptions.map((o) => [o, o])
+      : Object.entries(params.selectOptions)}
+    <select
+      {disabled}
+      {id}
+      bind:value={params.bindGetter, params.bindSetter}
+      {onclick}
+    >
+      {#each opts as opt}
+        <option value={opt[0]}>{opt[1]}</option>
+      {/each}
+    </select>
+  {:else if params.type === "textarea"}
+    <textarea
+      {disabled}
+      {id}
+      class={innerClass}
+      bind:value={params.bindGetter, params.bindSetter}
+      {onclick}
+    ></textarea>
+  {:else if params.type === "checkbox"}
+    <input
+      {disabled}
+      {id}
+      type="checkbox"
+      bind:checked={params.bindGetter, params.bindSetter}
+      {onclick}
+    />
+  {:else if params.type === "url"}
+    {#if disabled}
+      <a
+        href={"https://" + params.bindGetter().toString()}
+        class={["text-blue-600 underline cursor-pointer", innerClass]}
+        target="_blank">{params.bindGetter().toString()}</a
+      >
+    {:else}
+      <input
+        {disabled}
+        {id}
+        class={innerClass}
+        type={params.type}
+        bind:value={params.bindGetter, params.bindSetter}
+        {onclick}
+      />
+    {/if}
   {:else}
     <input
       {disabled}
       {id}
       class={innerClass}
       type={params.type}
+      min={params.min instanceof Date
+        ? params.min.toISOString().substring(0, 19)
+        : params.min}
+      max={params.max instanceof Date
+        ? params.max.toISOString().substring(0, 19)
+        : params.max}
+      step={params.step instanceof Date
+        ? params.step.toISOString().substring(0, 19)
+        : params.step}
       bind:value={params.bindGetter, params.bindSetter}
       {onclick}
     />
   {/if}
-{:else}
-  <input
-    {disabled}
-    {id}
-    class={innerClass}
-    type={params.type}
-    min={params.min instanceof Date
-      ? params.min.toISOString().substring(0, 19)
-      : params.min}
-    max={params.max instanceof Date
-      ? params.max.toISOString().substring(0, 19)
-      : params.max}
-    step={params.step instanceof Date
-      ? params.step.toISOString().substring(0, 19)
-      : params.step}
-    bind:value={params.bindGetter, params.bindSetter}
-    {onclick}
-  />
-{/if}
+</div>
