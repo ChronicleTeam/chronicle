7c0f2f9db4b10fcc7ffc0aef39dfd34b171def09
J-Dufour <dufour.dev@proton.me>
2025-09-18 16:25:41 -0400

    feat: rewrite registration page

--- frontend/src/routes/register/+page.svelte
+++ frontend/src/routes/register/+page.svelte
@@ -34,28 +34,28 @@
 </script>
 
 <div
-  class="flex flex-col justify-center items-center h-screen gap-6 bg-gray-100"
+  class="flex flex-col justify-center items-center h-screen gap-6 bg-base-200"
 >
   <img src="/logo.png" alt="Logo" class="h-20 mb-4" />
-  <div
-    class="flex flex-col items-center bg-white p-6 rounded-lg shadow-lg w-80"
-  >
-    <h2 class="text-2xl font-semibold mb-4">Register</h2>
-
-    <form on:submit|preventDefault={handleRegister} class="w-full">
-      <div class="mb-4">
+  <div class="card bg-base-100 shadow-lg w-80">
+    <form
+      on:submit|preventDefault={handleRegister}
+      class="card-body flex flex-col gap-4"
+    >
+      <h2 class="card-title text-2xl font-semibold mb-4">Register</h2>
+      <div>
         <label for="email" class="block text-sm font-semibold">Email</label>
         <input
           type="email"
           id="email"
           bind:value={email}
           placeholder="Enter your email"
-          class="w-full p-2 mt-1 border rounded-md"
+          class="input"
           required
         />
       </div>
 
-      <div class="mb-4">
+      <div>
         <label for="password" class="block text-sm font-semibold"
           >Password</label
         >
@@ -64,12 +64,12 @@
           id="password"
           bind:value={password}
           placeholder="Enter a password"
-          class="w-full p-2 mt-1 border rounded-md"
+          class="input"
           required
         />
       </div>
 
-      <div class="mb-6">
+      <div>
         <label for="confirmPassword" class="block text-sm font-semibold"
           >Confirm Password</label
         >
@@ -78,26 +78,18 @@
           id="confirmPassword"
           bind:value={confirmPassword}
           placeholder="Confirm your password"
-          class="w-full p-2 mt-1 border rounded-md"
+          class="input"
           required
         />
       </div>
 
-      <button
-        type="submit"
-        class="w-full py-2 bg-sky-700 text-white hover:bg-sky-900 rounded-md transition-all duration-300"
-      >
+      <button type="submit" class="btn btn-primary btn-block">
         Register
       </button>
 
-      <button
-        type="button"
-        on:click={() => goto("/")}
-        class="w-full mt-3 py-2 bg-gray-300 text-black hover:bg-gray-400 rounded-md transition-all duration-300"
-      >
+      <button type="button" on:click={() => goto("/")} class="btn btn-block">
         Back to Login
       </button>
     </form>
   </div>
 </div>
-
