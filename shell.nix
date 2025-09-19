43ec23409bebc627e1d78e4b958f3a2bb6158b5b
Remi1095 <remi.grosjean1@gmail.com>
2025-09-18 22:03:19 -0400

    In the process of creating Terraform for GCP

--- shell.nix
+++ shell.nix
@@ -1,9 +1,11 @@
 {
-  pkgs ? import <nixpkgs> { },
+  pkgs ? import <nixpkgs> { config.allowUnfree = true; },
 }:
 with pkgs;
 let
-  unstable = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") { };
+  unstable =
+    import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz")
+      { };
   cache = toString ./.nix-files;
   rustToolchain = "stable";
   nodejs = nodejs_22;
@@ -17,12 +19,14 @@ mkShell rec {
     nodejs
     nodePackages.npm
     postgresql_17
+    google-cloud-sdk
+    terraform
   ];
 
   RUSTUP_TOOLCHAIN = rustToolchain;
   RUSTUP_HOME = "${cache}/.rustup";
   CARGO_HOME = "${cache}/.cargo";
-  
+
   shellHook = ''
     export LD_LIBRARY_PATH=${lib.makeLibraryPath [ stdenv.cc.cc ]}
   '';
