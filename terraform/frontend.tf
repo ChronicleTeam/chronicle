119b79bc677062d5e6b137f78e590cab578e2a09
Remi1095 <remi.grosjean1@gmail.com>
2025-09-29 23:06:04 -0400

    Frontend cloud build

--- terraform/frontend.tf
+++ terraform/frontend.tf
@@ -58,3 +58,76 @@ resource "google_cloud_run_v2_service_iam_member" "public_frontend" {
   role     = "roles/run.invoker"
   member   = "allUsers"
 }
+
+resource "google_cloudbuild_trigger" "frontend" {
+  name            = "${var.frontend.service_name}-trigger"
+  service_account = google_service_account.frontend_build.id
+  github {
+    owner = var.github.username
+    name  = var.github.repo
+    push {
+      branch = "main"
+    }
+  }
+  included_files = ["frontend/**"]
+  substitutions = {
+    _IMAGE_URL    = var.frontend.image_url
+    _SERVICE_NAME = var.frontend.service_name
+    _REGION       = var.region
+  }
+  build {
+    timeout = "3600s"
+    options {
+      logging      = "CLOUD_LOGGING_ONLY"
+      machine_type = "E2_HIGHCPU_8"
+    }
+    step {
+      name = "gcr.io/cloud-builders/docker"
+      args = ["build", "-t", "$_IMAGE_URL:$COMMIT_SHA", "frontend"]
+    }
+    step {
+      name = "gcr.io/cloud-builders/docker"
+      args = ["push", "$_IMAGE_URL:$COMMIT_SHA"]
+    }
+    step {
+      name       = "gcr.io/google.com/cloudsdktool/cloud-sdk"
+      entrypoint = "gcloud"
+      args = [
+        "run", "deploy", "$_SERVICE_NAME",
+        "--image", "$_IMAGE_URL:$COMMIT_SHA",
+        "--region", "$_REGION",
+        "--platform", "managed",
+        "--quiet"
+      ]
+    }
+  }
+}
+
+resource "google_service_account" "frontend_build" {
+  account_id   = "${var.frontend.service_name}-build"
+  display_name = "Chronicle back-end"
+}
+
+resource "google_project_iam_member" "frontend_act_as" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountUser"
+  member  = "serviceAccount:${google_service_account.frontend_build.email}"
+}
+
+resource "google_project_iam_member" "frontend_logs_writer" {
+  project = var.project_id
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${google_service_account.frontend_build.email}"
+}
+
+resource "google_project_iam_member" "frontend_artifact_registry_writer" {
+  project = var.project_id
+  role    = "roles/artifactregistry.writer"
+  member  = "serviceAccount:${google_service_account.frontend_build.email}"
+}
+
+resource "google_project_iam_member" "frontend_cloud_run_admin" {
+  project = var.project_id
+  role    = "roles/run.admin"
+  member  = "serviceAccount:${google_service_account.frontend_build.email}"
+}
